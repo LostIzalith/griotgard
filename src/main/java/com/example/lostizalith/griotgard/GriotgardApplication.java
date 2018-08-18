2cb03f88413659ad8d69f1965d354d7d925250c9
solairerove <hax2033@gmail.com>
2018-08-18 22:37:44 +0300

    feature: create simple greeting http api

---
+++ src/main/java/com/example/lostizalith/griotgard/GriotgardApplication.java
@@ -0,0 +1,12 @@
+package com.example.lostizalith.griotgard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriotgardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriotgardApplication.class, args);
+	}
+}
